0218674118e2094c493c7dba92a8ca3687f806f5
faiuwle <dragonflykes@gmail.com>
2014-03-15 01:29:14 -0700

    added clear.sql

---
+++ clear.sql
@@ -0,0 +1,54 @@
+-- Special Note: Don't mess with this file; in particular, all the SQL statements
+-- must have a blank line after them, or my rather stupid parsing loop will barf.
+
+-- Statements for clearing the database:
+
+delete from FeatureBundleWord;
+
+delete from NaturalClassWord;
+
+delete from WordFeatureSet;
+
+delete from WordSubfeature;
+
+delete from WordFeatureDef;
+
+delete from CodaSupra;
+
+delete from Coda;
+
+delete from PeakSupra;
+
+delete from Peak;
+
+delete from OnsetSupra;
+
+delete from Onset;
+
+delete from SyllableSupra;
+
+delete from Word;
+
+delete from LegalCoda;
+
+delete from LegalPeak;
+
+delete from LegalOnset;
+
+delete from SupraApplies;
+
+delete from Suprasegmental;
+
+delete from FeatureBundlePhon;
+
+delete from NaturalClassPhon;
+
+delete from PhonemeFeatureSet;
+
+delete from PhonemeSubfeature;
+
+delete from PhonemeFeatureDef;
+
+delete from PhonemeSpelling;
+
+delete from Phoneme;
